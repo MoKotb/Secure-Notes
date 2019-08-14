3741efcfea88a59b2b38fbbe58ee7c6b85d55e52
Mohamed Kotb <mo.kotb96@gmail.com>
2019-08-15 01:14:45 +0200

    Setup Project

---
+++ Secure Notes/Controller/CreateNoteVC.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class CreateNoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Secure Notes/Controller/NotesVC.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class NotesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
