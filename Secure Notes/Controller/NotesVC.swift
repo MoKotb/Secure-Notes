7951a44cbf06d5f2e01183d3ab718d74d4a631a4
Mohamed Kotb <mo.kotb96@gmail.com>
2019-08-15 07:58:08 +0200

    Touch ID / Face ID Feature To Unlock Secure Note

--- Secure Notes/Controller/NotesVC.swift
+++ Secure Notes/Controller/NotesVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import LocalAuthentication
 
 class NotesVC: UIViewController {
 
@@ -41,6 +42,44 @@ class NotesVC: UIViewController {
         guard let createNoteVC = storyboard?.instantiateViewController(withIdentifier: CREATE_NOTE_VC_IDENTIFIER) as? CreateNoteVC else { return }
         navigationController?.pushViewController(createNoteVC, animated: true)
     }
+    
+    private func authenticateBiometrics(completion:@escaping CompletionHandler){
+        let myContext = LAContext()
+        let myLocalizedReasonString = "Secure Notes App use Touch ID / Face ID to secure your notes"
+        var authError:NSError?
+        if #available(iOS 8.0 , *){
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError){
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { (success, error) in
+                    if success {
+                        completion(true)
+                    }else{
+                        guard let errorMessage = error?.localizedDescription else { return }
+                        self.showErrorMessage(message: errorMessage)
+                        completion(false)
+                    }
+                }
+            }else{
+                guard let errorMessage = authError?.localizedDescription else { return }
+                self.showErrorMessage(message: errorMessage)
+                completion(false)
+            }
+        }else{
+            completion(false)
+        }
+    }
+    
+    private func showErrorMessage(message:String){
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func prepareForNoteDetails(note:Note){
+        guard let noteDetailsVC = storyboard?.instantiateViewController(withIdentifier: NOTE_DETAILS_VC_IDENTIFIER) as? NoteDetailsVC else { return }
+        noteDetailsVC.initData(note: note)
+        navigationController?.pushViewController(noteDetailsVC, animated: true)
+    }
 }
 
 extension NotesVC: UITableViewDelegate , UITableViewDataSource {
@@ -64,9 +103,21 @@ extension NotesVC: UITableViewDelegate , UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let note = self.notesArray[indexPath.row]
-        guard let noteDetailsVC = storyboard?.instantiateViewController(withIdentifier: NOTE_DETAILS_VC_IDENTIFIER) as? NoteDetailsVC else { return }
-        noteDetailsVC.initData(note: note)
-        navigationController?.pushViewController(noteDetailsVC, animated: true)
+        if note.lock{
+            authenticateBiometrics { (success) in
+                if success {
+                    DataService.sharedInstance.updateNote(note: note, isLocked: false, completion: { (success) in
+                        if success {
+                            DispatchQueue.main.async {
+                                self.prepareForNoteDetails(note: note)
+                            }
+                        }
+                    })
+                }
+            }
+        }else{
+            prepareForNoteDetails(note: note)
+        }
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
